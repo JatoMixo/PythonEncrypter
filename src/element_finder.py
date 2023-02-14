ec6c4aa93b67ddf34d388ff26afc78559ac4c544
JatoMixo <jatomixo@outlook.es>
2023-02-14 16:22:03 +0100

    main

--- src/element_finder.py
+++ src/element_finder.py
@@ -3,3 +3,5 @@ def find_by_second_element(dictionary, element):
   for i in dictionary:
     if dictionary[i] == element:
       return i
+  
+  raise Exception("Exception: Didn't find element in list.")
